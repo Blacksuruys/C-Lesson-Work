28f326940517453464b8ae7fa4406032e6faf5d4
blacksuruys <blacksuruys@mail.ru>
2023-04-11 21:23:01 +0800

    Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB * numberB){
+    Console.WriteLine("ДА");
+}
+else {
+    Console.WriteLine("НЕТ");}
