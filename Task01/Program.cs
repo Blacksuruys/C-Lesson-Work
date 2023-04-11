73f9011ed33c21b0918d53037cbd5e50c1c98532
blacksuruys <blacksuruys@mail.ru>
2023-04-11 21:52:39 +0800

    New Lesson

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -2,7 +2,7 @@
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите целое число");
 int numberB = Convert.ToInt32(Console.ReadLine());
-if (numberA == numberB * numberB){
+if ((numberA == numberB * numberB) | (numberB == numberA* numberA)){
     Console.WriteLine("ДА");
 }
 else {
