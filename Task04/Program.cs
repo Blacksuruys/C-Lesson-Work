8a973056f8db452a798839a7e60cf63e80c948a7
blacksuruys <blacksuruys@mail.ru>
2023-04-14 11:46:09 +0800

    end

---
+++ Task04/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите три числа, для обнаружения Max: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+if (numberA > max) ;
+{
+    numberA = max;
+}
+if (numberB > max) ;
+{
+    numberB = max;
+}
+Console.WriteLine($"Максимальное число = {max}");
---
+++ Task05/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите целое число, для получения диапазона чисел: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int count = -numberA;
+while (count <= numberA)
+{
+Console.Write(count + " ");
+count++;
+}
