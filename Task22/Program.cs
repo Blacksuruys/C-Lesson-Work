5f0dfb5037febae286e384863e39eb47dcfbf636
blacksuruys <blacksuruys@mail.ru>
2023-04-16 11:33:43 +0800

    Lesson 9

---
+++ Task22/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая
+//принимает на вход число (N) и выдаёт таблицу
+//квадратов чисел от 1 до N.
+
+void TableAllSquares(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index, 5}  {index * index, 5}");
+        index++;
+    }
+}
+
+Console.WriteLine("Please enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableAllSquares(number);
