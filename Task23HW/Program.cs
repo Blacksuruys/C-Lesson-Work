6e0b5ba15ed2fd91a99cb593be0906326f601c64
blacksuruys <blacksuruys@mail.ru>
2023-04-19 20:26:24 +0800

    Task23

---
+++ Task23HW/Program.cs
@@ -0,0 +1,17 @@
+﻿void Cube (int num)
+{
+   int index = 1;
+   while (index <= num)
+   {
+    Console.WriteLine($"{index , 5}  {index * index * index , 5}");
+    index++;
+   }
+   
+}
+
+
+
+Console.WriteLine("Введите число для определения x^3");
+int number = Convert.ToInt32(Console.ReadLine());
+Cube(number);
+
