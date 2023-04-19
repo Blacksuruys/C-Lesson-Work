d183f02b06ecd14932cf85c219d0e1ebbd7e67d3
blacksuruys <blacksuruys@mail.ru>
2023-04-19 20:06:30 +0800

    Task19

---
+++ Task19HW/Program.cs
@@ -0,0 +1,22 @@
+﻿bool Palindrom(int num)
+{
+    int num5 = num % 10;
+    int num1 = num / 10000;
+    int num4 = num % 100 / 10;
+    int num2 = num / 1000 % 10;
+    if ((num5 == num1) && (num4 == num2))
+        return true;
+    else
+        return false;
+}
+
+Console.WriteLine("Введите число для проверки на паллиндром: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA < 0) numberA = -numberA;
+if (numberA>9999 && numberA<100000)
+{
+    bool palindrom = Palindrom(numberA);
+    Console.WriteLine(palindrom ? "Да" : "Нет");
+}
+else
+    Console.WriteLine("Вы ввели неправильное число");
