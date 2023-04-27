26dd0411c152722dc23c9f4a1b81d7ab27e75454
blacksuruys <blacksuruys@mail.ru>
2023-04-27 20:52:12 +0800

    HomeWork 34+36+38

---
+++ Task31/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int SumPositiveElrmrnts(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) sum += arr[i];
+    }
+    return sum;
+}
+
+int SumNegativElements(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0) sum += arr[i];
+    }
+    return sum;
+}
+
+
+int[] array = CreateArrayRndInt(12, -9, 9);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
+int sumPos = SumPositiveElrmrnts(array);
+int sumNeg = SumNegativElements(array);
+Console.WriteLine($"Сумма положительных чисел в массиве равна = {sumPos}");
+Console.WriteLine($"Сумма отрицательных чисел в массиве равна = {sumNeg}");
---
+++ Task33/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+bool CheckNumber(int num, int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Please enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(5, 1, 90);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+bool checkNumber = CheckNumber(number, array);
+Console.WriteLine();
+Console.WriteLine(checkNumber ? "Есть соответствие" : "Соответствия нет");
