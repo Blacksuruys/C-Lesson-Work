1f6cd1e899b3af7e416d7e2fbb997eeaa97a2180
blacksuruys <blacksuruys@mail.ru>
2023-05-04 19:13:01 +0800

    HomeWMay

---
+++ Task47HW/Program.cs
@@ -0,0 +1,33 @@
+﻿double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * (max - min) + min, 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = CreateMatrixRndDouble(rows, columns, -100, 100);
+PrintMatrix(matrix);
---
+++ Task50HW/Program.cs
@@ -0,0 +1,42 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindSpecificNumber(int rows, int columns, int[,] matrix)
+{
+    if (rows < matrix.GetLength(0) && columns < matrix.GetLength(1) && rows >= 0 && columns >= 0)
+        Console.WriteLine(matrix[rows, columns]);
+    else
+        Console.WriteLine("Такого элемента в массиве нет");
+}
+
+Console.WriteLine("Введите позицию строки");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите позицию столбца");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = CreateMatrixRndInt(4, 4, -10, 10);
+PrintMatrix(matrix);
+FindSpecificNumber(row, column, matrix);
