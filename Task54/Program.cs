5b3a002c608fe9b67860755e6da9d19eb3e857a7
blacksuruys <blacksuruys@mail.ru>
2023-05-25 19:28:14 +0800

    HW lesson 8

---
+++ Task54/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+
+// элементы каждой строки двумерного массива.
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, j] > matrix[i, k])
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = CreateMatrixRndInt(3, 6, -10, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+SortMatrix(matrix);
+PrintMatrix(matrix);
---
+++ Task62/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4. 
+// Например, на выходе получается вот такой массив:
+
+int Creatematrix(int rows, int columns);
+{
+    int[,] spiralArray = new int[rows, columns];
+
+    int value = 1;
+    int minRow = 0;
+    int maxRow = spiralArray.GetLength(0) - 1;
+    int minCol = 0;
+    int maxCol = spiralArray.GetLength(1) - 1;
+}
+
+Void SpiralMatrix(int[,] matrix);
+{
+    while (value <= spiralArray.GetLength(0) * spiralArray.GetLength(1))
+    {
+        for (int i = minCol; i <= maxCol; i++)
+        {
+            spiralArray[minRow, i] = value++;
+        }
+        minRow++;
+
+        for (int i = minRow; i <= maxRow; i++)
+        {
+            spiralArray[i, maxCol] = value++;
+        }
+        maxCol--;
+
+        for (int i = maxCol; i >= minCol; i--)
+        {
+            spiralArray[maxRow, i] = value++;
+        }
+        maxRow--;
+
+        for (int i = maxRow; i >= minRow; i--)
+        {
+            spiralArray[i, minCol] = value++;
+        }
+        minCol++;
+    }
+}
+void PrintMatrix(int matrix);
+{
+    for (int i = 0; i < spiralArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < spiralArray.GetLength(1); j++)
+        {
+            Console.Write("{0:00} ", spiralArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+int matrix = Creatematrix(5, 5);
+SpiralMatrix(matrix);
+PrintMatrix(matrix);
