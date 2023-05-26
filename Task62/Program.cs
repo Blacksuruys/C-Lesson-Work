5960d0f5fd9bcf0be92c0461619db2e4a90998e6
blacksuruys <blacksuruys@mail.ru>
2023-05-26 19:41:43 +0800

    Created HW rev

--- Task62/Program.cs
+++ Task62/Program.cs
@@ -1,57 +1,52 @@
 ﻿// Напишите программу, которая заполнит спирально массив 4 на 4. 
 // Например, на выходе получается вот такой массив:
 
-int Creatematrix(int rows, int columns);
+class Program
 {
-    int[,] spiralArray = new int[rows, columns];
-
-    int value = 1;
-    int minRow = 0;
-    int maxRow = spiralArray.GetLength(0) - 1;
-    int minCol = 0;
-    int maxCol = spiralArray.GetLength(1) - 1;
-}
-
-Void SpiralMatrix(int[,] matrix);
-{
-    while (value <= spiralArray.GetLength(0) * spiralArray.GetLength(1))
+    static void Main(string[] args)
     {
-        for (int i = minCol; i <= maxCol; i++)
-        {
-            spiralArray[minRow, i] = value++;
-        }
-        minRow++;
+        int[,] spiralArray = new int[4, 4];
 
-        for (int i = minRow; i <= maxRow; i++)
-        {
-            spiralArray[i, maxCol] = value++;
-        }
-        maxCol--;
+        int value = 1;
+        int minRow = 0;
+        int maxRow = spiralArray.GetLength(0) - 1;
+        int minCol = 0;
+        int maxCol = spiralArray.GetLength(1) - 1;
 
-        for (int i = maxCol; i >= minCol; i--)
+        while (value <= spiralArray.GetLength(0) * spiralArray.GetLength(1))
         {
-            spiralArray[maxRow, i] = value++;
-        }
-        maxRow--;
+            for (int i = minCol; i <= maxCol; i++)
+            {
+                spiralArray[minRow, i] = value++;
+            }
+            minRow++;
 
-        for (int i = maxRow; i >= minRow; i--)
-        {
-            spiralArray[i, minCol] = value++;
+            for (int i = minRow; i <= maxRow; i++)
+            {
+                spiralArray[i, maxCol] = value++;
+            }
+            maxCol--;
+
+            for (int i = maxCol; i >= minCol; i--)
+            {
+                spiralArray[maxRow, i] = value++;
+            }
+            maxRow--;
+
+            for (int i = maxRow; i >= minRow; i--)
+            {
+                spiralArray[i, minCol] = value++;
+            }
+            minCol++;
         }
-        minCol++;
+
+        for (int i = 0; i < spiralArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < spiralArray.GetLength(1); j++)
+            {
+                Console.Write("{0:00} ", spiralArray[i, j]);
+            }
+            Console.WriteLine();
+        }
     }
 }
-void PrintMatrix(int matrix);
-{
-    for (int i = 0; i < spiralArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < spiralArray.GetLength(1); j++)
-        {
-            Console.Write("{0:00} ", spiralArray[i, j]);
-        }
-        Console.WriteLine();
-    }
-}
-int matrix = Creatematrix(5, 5);
-SpiralMatrix(matrix);
-PrintMatrix(matrix);
