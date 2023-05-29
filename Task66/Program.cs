4f20965fd67de8fded728fde9ca1cfe13962300b
blacksuruys <blacksuruys@mail.ru>
2023-05-29 19:38:28 +0800

    Hw

---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+
+// натуральных элементов в промежутке от M до N.
+
+
+
+Console.WriteLine("Введите целое положительное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int sumNumbers = SumNumbers(numberA, numberB);
+Console.WriteLine(sumNumbers);
+
+int SumNumbers(int numA, int numB)
+{
+    if (numA == numB) return numB;
+    else return numA + SumNumbers(numA + 1, numB);
+}
---
+++ Task69/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+
+
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int degreeNumbers = DegreeNumbers(numberA, numberB);
+Console.WriteLine(degreeNumbers);
+
+int DegreeNumbers(int numA, int numB)
+{
+    if (numB == 0) return 1;
+    return numA * DegreeNumbers(numA, numB - 1);
+}
